0b3698a937a0eaaa67ea4749815dd0818e3690d5
Dias <orazbaevdev@gmail.com>
2022-06-07 00:27:28 +0600

    append work with config

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 
 	restgin "github.com/DiasOrazbaev/RestGIN"
 	"github.com/DiasOrazbaev/RestGIN/pkg/handler"
 	"github.com/DiasOrazbaev/RestGIN/pkg/repository"
 	"github.com/DiasOrazbaev/RestGIN/pkg/service"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -15,9 +18,30 @@ func main() {
 		log.Fatalf("Failed init config: %s\n", err.Error())
 	}
 
-	repos := repository.NewRepository()
-	service := service.NewService(repos)
-	handlers := handler.NewHandler(service)
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Failed to loading env variables: %s\n", err.Error())
+	}
+
+	log.Println("Config correct initialized")
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to connection to db: %s\n", err.Error())
+	}
+
+	log.Println("Correctly connected to DB")
+
+	repos := repository.NewRepository(db)
+	newService := service.NewService(repos)
+	handlers := handler.NewHandler(newService)
 
 	server := new(restgin.Server)
 	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
